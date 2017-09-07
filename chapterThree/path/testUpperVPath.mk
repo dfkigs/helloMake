d73d5e30628c17009a0abc76bdf6b1f05114b8da
youngjack <dfkigs@163.com>
2017-09-07 15:10:41 +0800

    update VPATH

--- chapterThree/path/testUpperVPath.mk
+++ chapterThree/path/testUpperVPath.mk
@@ -1,6 +1,15 @@
+# very important: VPATH is valid in Makefile !!
+# so you can look at error and ok .
+# and think !
+
 VPATH = ./: ./src
 
-main : main.c
+# main.c in prerequisites is ok.  will replaced it with ./src/main.c
+error : main.c
+# main.c in below line is not ok, it will be passed to shell as main.c
+	cc     main.c   -o main
+
+ok : main.c
 	cc     $^   -o main
 
 clean :
